d0ef8f2acdc893174a4c4bd17670d24a40427ae5
Andre <andrekok@discovery.bank>
2024-02-26 11:12:47 +0200

    Add count down

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  public accomplished: number = 20
+  public currentDate: any;
+  public targetDate: any;
+  public cDateMillisecs: any;
+  public tDateMillisecs: any;
+  public difference: any;
+  public seconds: any;
+  public minutes: any;
+  public hours: any;
+  public days: any;
+  public displayDays: any;
+  public displayHours: any;
+  public displayMinutes: any;
+  public displaySeconds: any;
+
+
+  ngOnInit(): void {
+    this.myTimer();
+  }
+
+  public myTimer() {
+    this.currentDate = new Date();
+    this.targetDate = new Date(2024, 7, 27);
+    this.cDateMillisecs = this.currentDate.getTime();
+    this.tDateMillisecs = this.targetDate.getTime();
+    this.difference = this.tDateMillisecs - this.cDateMillisecs;
+    this.seconds = Math.floor(this.difference / 1000);
+    this.minutes = Math.floor(this.seconds / 60);
+    this.hours = Math.floor(this.minutes / 60);
+    this.days = Math.floor(this.hours / 24);
+
+    this.hours %= 24;
+    this.minutes %= 60;
+    this.seconds %= 60;
+    this.hours = this.hours < 10 ? "0" + this.hours : this.hours;
+    this.minutes = this.minutes < 10 ? "0" + this.minutes : this.minutes;
+    this.seconds = this.seconds < 10 ? "0" + this.seconds : this.seconds;
+
+    this.displayDays = this.days;
+    this.displayHours = (Number(this.hours) + 6).toString();
+    this.displayMinutes = this.minutes;
+    this.displaySeconds = this.seconds;
+
+    setInterval(this.myTimer, 1000);
+  }
 
 }
